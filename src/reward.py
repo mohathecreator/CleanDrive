45c1d06fe5cec3dd8a79d41f791a408621e7e06c
Mohamed El Domyaty <mohaeldomyatty@gmail.com>
2026-06-19 11:34:51 +0200

    feat: finish rewardstrategy class with abstractmethod decorator and subclass speedrewardstrategy with wreward logic

--- src/reward.py
+++ src/reward.py
@@ -1,6 +1,20 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 
-@abstractmethod
-class RewardStrategy:
-    pass
+class RewardStrategy(ABC):
+    @abstractmethod
+    def compute(self, observation, action, info) -> float:
+        pass
+
+
+class SpeedRewardStrategy(RewardStrategy):
+    def __init__(self, speed_weigth: float = 1.0,
+                 crash_penalty: float = -10.0):
+        self.speed_weight = speed_weigth
+        self.crash_penalty = crash_penalty
+
+    def compute(self, observation, action, info) -> float:
+        reward = self.speed_weight * info["velocity"]
+        if info["crash"] or info["out_of_road"]:
+            reward += self.crash_penalty
+        return reward
