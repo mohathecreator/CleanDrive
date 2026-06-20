76842f75f6573dec08fbc10e9e76d0d3feac5ed4
Mohamed El Domyaty <mohaeldomyatty@gmail.com>
2026-06-20 11:03:09 +0200

    feat: create TrainingOrchestrator class with constructor and run method

---
+++ src/orchestrator.py
@@ -0,0 +1,15 @@
+from config import TrainingConfig
+from environment import EnvironmentWrapper
+from agent import AgentManager
+
+
+class TrainingOrchestrator:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+        self.environment = EnvironmentWrapper(config)
+        self.agent_manager = AgentManager()
+
+    def run(self):
+        self.agent_manager.create_model(self.environment)
+        self.agent_manager.train(self.config.total_timesteps)
+        self.agent_manager.save(self.config.checkpoint_path / "ppo_model")
