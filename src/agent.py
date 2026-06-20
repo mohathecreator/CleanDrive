957449e6b5b822313bb249ee6200b1c7674cf008
Mohamed El Domyaty <mohaeldomyatty@gmail.com>
2026-06-20 10:33:45 +0200

    feat: add logic to train, save and load methods

--- src/agent.py
+++ src/agent.py
@@ -8,9 +8,16 @@ class AgentManager:
     def create_model(self, env):
         self.model = PPO("MlpPolicy", env)
 
-    def train():
-        
+    def train(self, total_timesteps):
+        if self.model is None:
+            raise RuntimeError("create_model() needs "
+                               "to be called before train()")
+        self.model = self.model.learn(total_timesteps)
+        return self.model
 
-    def save():
+    def save(self, path):
+        return self.model.save(path)
 
-    def load():
+    def load(self, path):
+        self.model = PPO.load(path)
+        return self.model
