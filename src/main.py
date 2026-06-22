ae7761638b29c92e13925c230ab52c8ad9bbb993
Mohamed El Domyaty <mohaeldomyatty@gmail.com>
2026-06-22 11:53:21 +0200

    feat: add updated reward strategies to main script

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from config import TrainingConfig
-from reward import SpeedRewardStrategy
+from reward import (SpeedRewardStrategy, LaneCenteringRewardStrategy,
+                    AvoidCollisionRewardStrategy, BrakingRewardStrategy,
+                    CompositeRewardStrategy)
 from orchestrator import TrainingOrchestrator
 
 
 map_config = dict(use_render=False, manual_control=False,
                   num_scenarios=1, map="S",)
 
-reward_strategy = SpeedRewardStrategy()
+reward_strategy = CompositeRewardStrategy(strategies=[
+    SpeedRewardStrategy(),
+    LaneCenteringRewardStrategy(),
+    AvoidCollisionRewardStrategy(),
+    BrakingRewardStrategy(),
+])
 
 config = TrainingConfig(map_config=map_config,
                         reward_strategy=reward_strategy,
-                        total_timesteps=15)
+                        total_timesteps=100_000)
 
 orchestrator = TrainingOrchestrator(config)
 orchestrator.run()
